f30b82afd2b88c29c6141d5162cc44a5f0dbde5d
zsxoff <zsxoff@gmail.com>
2019-07-09 18:13:28 +0400

    googleFormat formatting

--- src/main/java/com/routine/summer/controllers/EmployeeController.java
+++ src/main/java/com/routine/summer/controllers/EmployeeController.java
@@ -2,10 +2,9 @@ package com.routine.summer.controllers;
 
 import com.routine.summer.entities.Employee;
 import com.routine.summer.services.EmployeeService;
+import java.util.List;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 public class EmployeeController {
@@ -16,27 +15,42 @@ public class EmployeeController {
         this.employeeService = service;
     }
 
-    @RequestMapping(value = "/employees/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(
+            value = "/employees/{id}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public Employee select(@PathVariable int id) {
         return employeeService.select(id);
     }
 
-    @RequestMapping(value = "/employees/all", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(
+            value = "/employees/all",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Employee> selectAll() {
         return employeeService.findAll();
     }
 
-    @RequestMapping(value = "/employees", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(
+            value = "/employees",
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public Employee create(@RequestBody Employee employee) {
         return employeeService.create(employee);
     }
 
-    @RequestMapping(value = "/employees", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(
+            value = "/employees",
+            method = RequestMethod.PUT,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public Employee update(@RequestBody Employee employee) {
         return employeeService.update(employee);
     }
 
-    @RequestMapping(value = "/employees/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(
+            value = "/employees/{id}",
+            method = RequestMethod.DELETE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public void delete(@PathVariable int id) {
         employeeService.delete(id);
     }
